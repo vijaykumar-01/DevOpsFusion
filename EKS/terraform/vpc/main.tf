713bc8e2cb38ab3f9704b8f9e24812d8c45c508a
vijaykumar-01 <163980312+vijaykumar-01@users.noreply.github.com>
2024-08-07 17:07:40 +0530

    Create main.tf

---
+++ EKS/terraform/vpc/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+}
